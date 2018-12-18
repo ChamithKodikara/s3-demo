d19ea443d3b873f8bc8d1d5dd4313f276f08b705
Chamith Kodikara <chamith@ceylonit.com>
2018-12-18 13:47:31 +0530

    -initial commit

---
+++ src/main/java/com/helixz/s3/demo/S3DemoApplication.java
@@ -0,0 +1,14 @@
+package com.helixz.s3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3DemoApplication.class, args);
+    }
+
+}
+
