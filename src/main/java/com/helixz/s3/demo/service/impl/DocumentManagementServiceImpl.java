ca2f4cf631603f47116501dcbc25f7283d21b0ed
Chamith Kodikara <chamith@ceylonit.com>
2019-01-03 13:46:31 +0530

    -file upload download delete from S3

--- src/main/java/com/helixz/s3/demo/service/impl/DocumentManagementServiceImpl.java
+++ src/main/java/com/helixz/s3/demo/service/impl/DocumentManagementServiceImpl.java
@@ -59,7 +59,7 @@ public class DocumentManagementServiceImpl implements DocumentManagementService 
         try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
             fos.write(file.getBytes());
         } catch (IOException e) {
-            log.error("Error converting multipart file to file", e);
+            log.error("Error converting multipartFile to file", e);
         }
         return convertedFile;
     }
